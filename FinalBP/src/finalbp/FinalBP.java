c2c5d9a001c6b1bf07f1d01f1664cd231fc4d822
satrioagnag <117004344+satrioagnag@users.noreply.github.com>
2022-12-23 23:25:51 -0800

    Add files via upload

--- FinalBP/src/finalbp/FinalBP.java
+++ FinalBP/src/finalbp/FinalBP.java
@@ -12,9 +12,15 @@ import java.util.ArrayList;
 public class FinalBP {
     private static int choice=0;
     private static Scanner input = new Scanner(System.in);
+    static String [] listProd = {"Cilok", "Kebab Keju", "Kebab Singkong Keju", "Cireng", "Tahu Bakso"};
+    static int [] listPrice = {12000, 20000, 22000, 15000, 23000};
+    static int [] cart = new int [5];
+    
+    
     static void Homepg(){
-        
-        System.out.println("Welcome to Frozen Agna Shopping System "
+    int cekvar = 0;
+        System.out.println("Welcome to Frozen Agna Information System "
+                + "\n=================" 
                 + "\n1. Cashier\n2. Admin\n3. Owner");
         System.out.println("Please input your token (numeric only");        
         int User = input.nextInt();
@@ -28,25 +34,29 @@ public class FinalBP {
     }
       static void Cashier(){ 
           System.out.println("Welcome, Cashier\n========================");
-          System.out.println("Menu:\n1. Cilok\n2. Kebab Keju\n3. Singkong Keju\n4. Cireng\n5. Tahu Bakso");
+          System.out.println("Menu: ");          
+          for (int i = 0; i < listProd.length; i++) {
+              System.out.println((i+1)+ ". " + listProd[i] + "  " + listPrice[i] );              
+          }
+      }
+          /*System.out.println("Menu:\n1. Cilok\n2. Kebab Keju\n3. Singkong Keju\n4. Cireng\n5. Tahu Bakso");
           
-          //declare barang
-          String [] listProd = {"Cilok", "Kebab Keju", "Kebab Singkong Keju", "Cireng", "Tahu Bakso"};
           int stop=0;
-          ArrayList ArrayCart;
-          ArrayCart = new ArrayList();
-          ArrayList<String> cart = new ArrayList<String>();
+          //ArrayList ArrayCart;
+          //ArrayCart = new ArrayList();
+         ArrayList<String> cart = new ArrayList<String>();
                         
          System.out.println("Input your item(s) (numeric only)");
          cart.add(input.nextLine());
           do {
            // System.out.println("Number of string in array: " + cart.size());
             cart.add(input.nextLine());
-            System.out.println("Input your next item or type 'end' to finish shopping");
+            System.out.println("Input your next item \n(type 'end' if you finish)");
       // Display eveything in the list
           } while (!input.hasNext("end"));
         displayList(cart);  
       }
+          
     
   
       static void displayList(ArrayList cart){
@@ -56,19 +66,22 @@ public class FinalBP {
           System.out.println("Number of string in array: " + cart.size());
       }
       
-         
-      static void Admin(){
+         */
+      
+        static void Admin(){
           System.out.println("ADmin cek");
       }
-      static void Owner(){
+        static void Owner(){
           System.out.println("Owner cek");
       }
+          
     
     
 
     /**
      * @param args the command line arguments
      */
+      
     public static void main(String[] args) {
         Homepg();
         if (choice==1) {
@@ -82,7 +95,7 @@ public class FinalBP {
         }
             
         
-   
+        //System.out.println(cekvar);
 // TODO code application logic here
     }
 }
